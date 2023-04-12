0c8edd75f02c7bf30e49685505e6ff33ae117a4b
Demetrios <jkopatsis@gmail.com>
2023-04-11 21:08:16 -0700

    personal improvements to functionality

--- parallax-scroll/script.js
+++ parallax-scroll/script.js
@@ -6,15 +6,16 @@ let hill5 = document.getElementById('hill5');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
+    if(value < 350){
+        text.style.marginTop = value * 2.5 + 'px';
 
-    text.style.marginTop = value * 2.5 + 'px';
-
-    leaf.style.top = value * -1.5 + 'px';
-    leaf.style.left = value * 1.5 + 'px';
-
-    hill5.style.left = value * 1.5 + 'px';
-    hill4.style.left = value * -1.5 + 'px';
-
-    hill1.style.top = value * 1.5 + 'px';
+        leaf.style.top = value * -1.5 + 'px';
+        leaf.style.left = value * 1.5 + 'px';
+    
+        hill5.style.left = value * 1.5 + 'px';
+        hill4.style.left = value * -1.5 + 'px';
+    
+        hill1.style.top = value * 1.5 + 'px';
+    }
 
 });
